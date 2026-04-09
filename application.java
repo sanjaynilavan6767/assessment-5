90876a373f631a20942b51194e0e791d4cc37b24
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-09 16:45:56 +0530

    application.java

---
+++ application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+@SpringBootApplication
+@RestController
+public class Application {
+    @GetMapping("/")
+    public String home() {
+        return "hello from spring boot docker";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

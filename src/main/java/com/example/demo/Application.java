2d8f7d73b074d862c43a6426f2e0c90502386f6f
sanjaynilavan6767 <sanjaynilvan.s2023@vitstudent.ac.in>
2026-04-09 17:12:30 +0530

    prt change

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,14 +1,18 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+
 @SpringBootApplication
 @RestController
 public class Application {
+
     @GetMapping("/")
     public String home() {
-        return "hello from spring boot docker";
+        return "Hello from Spring Boot Docker Jenkins";
     }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
